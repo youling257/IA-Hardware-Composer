a03209d6083749cccc9198a324d7e11a4c588386
youling257 <youling257@gmail.com>
2020-08-02 10:28:17 +0800

    Revert "iahwc2: fix format building errors"
    This reverts commit c454b88dcdb90f936ff6cc55e5f94a3a58d978f1.

--- os/android/iahwc2.cpp
+++ os/android/iahwc2.cpp
@@ -1041,10 +1041,10 @@ HWC2::Error IAHWC2::HwcDisplay::GetDisplayIdentificationData(
 
   if (display_) {
     if (display_->GetDisplayIdentificationData(outPort, outDataSize, outData)) {
-      (outData == NULL) ? ITRACE("outPort=%lx, outDataSize=%lx, outData=NULL",
-                                 (unsigned long)outPort, (unsigned long)outDataSize)
-                        : ITRACE("outPort=%lx, outDataSize=%lx, outData=%lx",
-                                 (unsigned long)outPort, (unsigned long)outDataSize, (unsigned long)outData);
+      (outData == NULL) ? ITRACE("outPort=%x, outDataSize=%x, outData=NULL",
+                                 outPort, outDataSize)
+                        : ITRACE("outPort=%x, outDataSize=%x, outData=%x",
+                                 outPort, outDataSize, outData);
 
       return HWC2::Error::None;
     }
